9cf4ce26c122eae0babc3c898090f59a229dffb3
Jamie Larchin <jamielarchin@gmail.com>
2020-05-05 20:48:41 +0100

    reset create react app, setup prettier and styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { StrictMode } from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0
+  }
+`
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  </StrictMode>,
+  document.getElementById("root")
+)
